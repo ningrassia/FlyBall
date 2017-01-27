7b5467d77ceaeab653ddccb0eb921e89469058a5
Nik Ingrassia <Nik Ingrassia>
2017-01-26 17:41:15 -0800

    Initial commit

---
+++ Assets/BallController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float force;
+    public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate ()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0, v);
+        RotateRelativeToCamera(move, cam);
+
+        rb.AddForce(move * force);
+    }
+
+    private void RotateRelativeToCamera(Vector3 direction, Camera cam)
+    {
+        // rotate given direction by the camera's rotation
+        Vector3 camDir = cam.transform.rotation * direction;
+
+        // add result to object's location to get relative direction
+        Vector3 objectDir = transform.position + camDir;
+
+        // create quaternion facing direction
+        Quaternion targetRotation = Quaternion.LookRotation(objectDir - transform.position);
+
+        // constrain rotation to the Y axis
+        Quaternion constrained = Quaternion.Euler(0.0f, targetRotation.eulerAngles.y, 0.0f);
+
+        // slerp rotation
+        transform.rotation = Quaternion.Slerp(transform.rotation, constrained, Time.deltaTime);
+    }
+}
---
+++ Assets/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject follow;
+    public float damping = 1;
+    private Vector3 follow_dist;
+    
+
+	// Use this for initialization
+	void Start () {
+        follow_dist = follow.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // LateUpdate runs after all updates
+    void LateUpdate ()
+    {
+        float angle = follow.transform.eulerAngles.y;
+        
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        transform.position = follow.transform.position - (rotation * follow_dist);
+        transform.LookAt(follow.transform);
+    }
+
+
+}
